9e2f85f46245f1ffc6203d0e38e4a61dfc0284bc
Luke Floden <luke.floden@gmail.com>
2024-12-11 16:43:06 -0500

    chore: injecting vercel speed insights into root layout

---
+++ src/routes/+layout.ts
@@ -0,0 +1,8 @@
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+// import type { LayoutLoad } from './$types';
+
+injectSpeedInsights();
+
+// export const load = (async () => {
+// 	return {};
+// }) satisfies LayoutLoad;
