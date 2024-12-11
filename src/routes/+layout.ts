de502d648e352a2c1c83b1f4a1d7e428430091b7
Luke Floden <luke.floden@gmail.com>
2024-12-11 17:37:35 -0500

    fuck it, why not try analytics as well

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,7 +1,10 @@
+import { dev } from '$app/environment';
+import { injectAnalytics } from '@vercel/analytics/sveltekit';
 import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 // import type { LayoutLoad } from './$types';
 
 injectSpeedInsights();
+injectAnalytics({ mode: dev ? 'development' : 'production' });
 
 // export const load = (async () => {
 // 	return {};
