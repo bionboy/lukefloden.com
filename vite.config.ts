1b3e1703f0a15773c1a9ce809b0c6b6c4bfbab64
Luke Floden <luke.floden@gmail.com>
2025-01-01 18:53:54 -0500

    feat: setup local ssl

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		basicSsl({
+			/** name of certification */
+			// name: 'test',
+			/** custom trust domains */
+			// domains: ['*.custom.com'],
+			/** custom certification directory */
+			// certDir: '/Users/.../.devServer/cert'
+		})
+	]
 });
