1602440e1920afcead7b077b70cd1b3467c4c64d
Luke Floden <luke.floden@gmail.com>
2025-01-05 13:30:09 -0500

    feat (WIP): create header and nav based on shadcn demo and remove existing one

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { cn } from '$lib/utils.js';
+
+	export type NavLocations = { route: string; name: string }[];
+
+	const defaultLocations = [
+		{ route: '/', name: 'Home' },
+		{ route: '/demos', name: 'Demos' },
+		{ route: '/about', name: 'About' },
+		{ route: '/contact', name: 'Contact' }
+	];
+
+	let { locations = defaultLocations }: { locations?: NavLocations } = $props();
+
+	// This is the excepted way to use props that use keywords in Svelte (https://svelte.dev/docs/svelte/legacy-export-let#Renaming-props)
+	let className: string | undefined | null = undefined;
+	export { className as class };
+</script>
+
+<nav class={cn('flex items-center space-x-4 lg:space-x-6', className)}>
+	{#each locations as { name, route }}
+		<a href={route}> {name} </a>
+	{/each}
+</nav>
+
+<style lang="postcss">
+	a {
+		@apply text-muted-foreground hover:text-primary text-sm font-medium transition-colors;
+	}
+</style>
