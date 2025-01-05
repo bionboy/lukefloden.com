4d01f2187751fe66344f16a150b08c8d7975d300
Luke Floden <luke.floden@gmail.com>
2025-01-05 13:31:07 -0500

    feat (WIP): move header from home to layout of all pages site-wide

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import '../app.pcss';
-
 	import type { Snippet } from 'svelte';
 	import type { LayoutData } from './$types';
+	import Header from '$lib/components/Header.svelte';
 
 	let { data, children }: { data: LayoutData; children: Snippet } = $props();
 </script>
 
+<Header />
 {@render children()}
