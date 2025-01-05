71c159315f3ccbf980b1a7442cfeba6d0f27f124
Luke Floden <luke.floden@gmail.com>
2025-01-05 15:04:02 -0500

    feat: recreate and improve on my demo gallery card w/ shad and move to components instead of snippets

--- src/routes/demos/+page.svelte
+++ src/routes/demos/+page.svelte
@@ -1,78 +1,38 @@
 <script lang="ts">
-	import type { Snippet } from 'svelte';
 	import type { PageData } from './$types';
 	import Parallax from './parallax/Parallax.svelte';
 	import AlwaysLevel from './always-level/AlwaysLevel.svelte';
+	import DemoCard from './DemoCard.svelte';
+
 	let { data }: { data: PageData } = $props();
 </script>
 
-{#snippet artwork(title: string, route: string, art: Snippet)}
-	<div class="artwork-container">
-		{#if title}
-			<a href={route}>
-				<h2>{title}</h2>
-			</a>
-		{/if}
-		<div class="artwork-content">
-			<a href={route}>
-				{@render art()}
-			</a>
-		</div>
-	</div>
-	<style>
-		.artwork-container {
-			aspect-ratio: 1 / 1;
-			display: flex;
-			flex-direction: column;
-			overflow: clip;
-
-			background-color: hsl(240, 100%, 80%);
-			border-radius: 2rem;
-
-			text-align: center;
-		}
-
-		h2 {
-			/* color: #333; */
-			margin: 10px;
-		}
-
-		/* link style reset, only for content, so original HTML link styles remain :) */
-		.artwork-content {
-			flex: 1;
-
-			> a {
-				color: inherit;
-				text-decoration: none;
-			}
-		}
-	</style>
-{/snippet}
-
-{#snippet parallax()}
-	<Parallax title="hi!" />
-{/snippet}
-
-{#snippet alwaysLevel()}
-	<div class="always-level-container">
-		<AlwaysLevel />
-	</div>
-	<style>
-		.always-level-container {
-			display: flex;
-			justify-content: center;
-			align-items: center;
-			height: 100%;
-			background-color: hsl(160, 100%, 90%);
-		}
-	</style>
-{/snippet}
-
 <div class="gallery">
-	{@render artwork('Parallax Hero', 'demos/parallax', parallax)}
-	{@render artwork('Always Level', 'demos/always-level', alwaysLevel)}
-	{#each { length: 2 } as _, index}
-		<div></div>
+	<DemoCard
+		title="Parallax Hero"
+		description="User controlled parallax effect"
+		route="demos/parallax"
+	>
+		<Parallax title="hi!" />
+	</DemoCard>
+	<DemoCard title="Always Level" description="asdf" route="demos/always-level">
+		<div class="always-level-container">
+			<AlwaysLevel />
+		</div>
+		<style>
+			.always-level-container {
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				height: 100%;
+				background-color: hsl(160, 100%, 90%);
+			}
+		</style>
+	</DemoCard>
+
+	<!-- * Fill in future spots, so for now our demo cards look less lonely  -->
+	{#each { length: 22 } as _, index}
+		<div class=" bg-accent size-full aspect-square"></div>
 	{/each}
 </div>
 
---
+++ src/routes/demos/DemoCard.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card/index.js';
+	import type { Snippet } from 'svelte';
+
+	let props: {
+		title?: string;
+		description?: string;
+		route?: string;
+		children?: Snippet;
+	} = $props();
+</script>
+
+<a href={props.route}>
+	<Card.Root class="flex flex-col aspect-square">
+		<Card.Header>
+			{#if props.title}
+				<Card.Title>{props.title}</Card.Title>
+			{/if}
+			{#if props.description}
+				<Card.Description>{props.description}</Card.Description>
+			{/if}
+		</Card.Header>
+		<Card.Content class=" flex-1 flex-grow ">
+			{@render props.children?.()}
+		</Card.Content>
+	</Card.Root>
+</a>
