82d27f70a72bc6d903a2e8f226d8482f2b20b76d
Luke Floden <luke.floden@gmail.com>
2025-01-13 17:30:57 -0500

    feat (WIP): init about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Card from '$lib/components/ui/card/card.svelte';
+	import type { PageData } from './$types';
+
+	let { data }: { data: PageData } = $props();
+</script>
+
+<div class="bg-secondary m-8 p-4 font-jersey15">
+	<h2>Me</h2>
+	<h2>This Site</h2>
+</div>
+
+<style lang="postcss">
+</style>
