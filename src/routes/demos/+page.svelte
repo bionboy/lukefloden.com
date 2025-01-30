9ec9809f5ab7143cfb00b9ed04f7dd59844b112d
Luke Floden <luke.floden@gmail.com>
2025-01-30 16:03:54 -0500

    feat (WIP): add tags to demo card footers

--- src/routes/demos/+page.svelte
+++ src/routes/demos/+page.svelte
@@ -2,9 +2,25 @@
 	import type { PageData } from './$types';
 	import Parallax from './parallax/Parallax.svelte';
 	import AlwaysLevel from './always-level/AlwaysLevel.svelte';
-	import DemoCard from './DemoCard.svelte';
+	import DemoCard, { type DemoTag } from './DemoCard.svelte';
+	import perlinPlane from '$lib/assets/images/perlin-plane.png';
+	import Badge from '$lib/components/ui/badge/badge.svelte';
+	import { ExternalLink } from 'lucide-svelte';
 
 	let { data }: { data: PageData } = $props();
+
+	const tags: Record<string, DemoTag> = {
+		github: {
+			name: 'GitHub',
+			icon: ExternalLink,
+			twColor: 'green-600'
+		},
+		external: {
+			name: 'External',
+			twColor: 'blue-400',
+			variant: 'secondary'
+		}
+	};
 </script>
 
 <div class="gallery bg-background">
@@ -12,6 +28,9 @@
 		title="Parallax Hero"
 		description="Parallax effect hero component, moving on scroll or mouse"
 		route="demos/parallax"
+		tags={[
+			{ ...tags.github, href: 'https://gist.github.com/bionboy/150a1f1c80a1df40737c5f0eec537b62' }
+		]}
 	>
 		<Parallax title="hi!" />
 	</DemoCard>
@@ -24,18 +43,28 @@
 			<AlwaysLevel />
 		</div>
 	</DemoCard>
+	<DemoCard
+		title="Perlin Plane"
+		description="An interactive 3d rendering of a 2d plane of 'perlin' noise. Made with three.js"
+		route="https://bionboy.github.io/PerlinPlane/"
+		tags={[{ ...tags.github, href: 'https://github.com/bionboy/PerlinPlane' }, tags.external]}
+	>
+		<div class="size-full flex place-items-center overflow-clip rounded">
+			<img
+				class="rounded"
+				alt="a 2d plane with generated hills based on a noise algorithm. The different heights correspond to different colors"
+				src={perlinPlane}
+			/>
+		</div>
+	</DemoCard>
 
 	<!-- * Fill in future spots, so for now our demo cards look less lonely  -->
-	{#each Array(22) as _}
-		<div class="bg-border size-full aspect-square"></div>
-	{/each}
+	<!-- {#each Array(3) as _} <div class="bg-border size-full aspect-square rounded-md"></div> {/each} -->
 </div>
 
-<style>
+<style lang="postcss">
 	.gallery {
-		display: grid;
+		@apply grid gap-4 m-5;
 		grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-		margin: 20px;
-		gap: 15px;
 	}
 </style>
--- src/routes/demos/DemoCard.svelte
+++ src/routes/demos/DemoCard.svelte
@@ -1,33 +1,65 @@
 <script lang="ts">
+	import AspectRatio from '$lib/components/ui/aspect-ratio/aspect-ratio.svelte';
+	import Badge from '$lib/components/ui/badge/badge.svelte';
 	import * as Card from '$lib/components/ui/card/index.js';
-	import type { Snippet } from 'svelte';
+	import type { Component, Snippet } from 'svelte';
+	import { ExternalLink } from 'lucide-svelte';
+
+	export interface DemoTag {
+		name: string;
+		twColor?: string;
+		icon?: Component;
+		variant?: string;
+		href?: string;
+	}
 
 	let props: {
 		title?: string;
 		description?: string;
 		route?: string;
+		tags?: DemoTag[];
 		children?: Snippet;
 	} = $props();
+	// $inspect(props.tags);
 </script>
 
 <a href={props.route}>
 	<Card.Root
 		class="
-		flex flex-col
-		aspect-square shadow-md shadow-border
+		shadow-md shadow-border
 		transition-transform hover:scale-105 
+		overflow-clip
 	"
 	>
-		<Card.Header>
-			{#if props.title}
-				<Card.Title>{props.title}</Card.Title>
+		<AspectRatio ratio={1} class="flex flex-col">
+			<Card.Header>
+				{#if props.title}
+					<Card.Title>{props.title}</Card.Title>
+				{/if}
+				{#if props.description}
+					<Card.Description>{props.description}</Card.Description>
+				{/if}
+			</Card.Header>
+			<Card.Content class="flex-grow min-h-0 min-w-0 p-0 m-6 overflow-clip rounded">
+				{@render props.children?.()}
+			</Card.Content>
+			{#if props.tags}
+				<Card.Footer class="gap-2 justify-end">
+					{#each props.tags as tag}
+						<Badge
+							class={`bg-${tag.twColor} hover:bg-${tag.twColor} hover:brightness-90`}
+							href={tag.href}
+							variant={tag.variant ?? 'default'}
+						>
+							{tag.name}
+							{#if tag.icon}
+								<!-- <icon size="1rem" class="ml-1"></icon> -->
+								<svelte:component this={tag.icon} size="1rem" class="ml-1"></svelte:component>
+							{/if}
+						</Badge>
+					{/each}
+				</Card.Footer>
 			{/if}
-			{#if props.description}
-				<Card.Description>{props.description}</Card.Description>
-			{/if}
-		</Card.Header>
-		<Card.Content class="flex-grow">
-			{@render props.children?.()}
-		</Card.Content>
+		</AspectRatio>
 	</Card.Root>
 </a>
