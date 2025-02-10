397f40973b6c88881029aa3331b526b26a0fd5c7
Luke Floden <luke.floden@gmail.com>
2025-02-10 16:05:50 -0500

    fix: redirect base url to home page instead of 404

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,16 +1,22 @@
 import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
-import { dev } from '$app/environment';
+// import { dev } from '$app/environment';
 
 export const load: LayoutServerLoad = async ({ url }) => {
+	middlewareRedirctFromBaseURL(url);
 	middlewareUnderConstruction(url);
 };
-// }) satisfies LayoutServerLoad;
 
 function middlewareUnderConstruction(url: URL) {
 	// const isUnderConstruction = !dev;
 	const isUnderConstruction = false;
 	if (isUnderConstruction && url.pathname !== '/under-construction') {
 		throw redirect(307, '/under-construction');
+	}
+}
+
+function middlewareRedirctFromBaseURL(url: URL) {
+	if (url.pathname === '/') {
+		throw redirect(302, '/home');
 	}
 }
