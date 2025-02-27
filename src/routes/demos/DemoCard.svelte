bc2137a89f1616b965a93ee0e58e4b48b0f253da
Luke Floden <luke.floden@gmail.com>
2025-02-27 18:29:35 -0500

    feat: add class injection, optional remove button to demo card

--- src/routes/demos/DemoCard.svelte
+++ src/routes/demos/DemoCard.svelte
@@ -3,6 +3,8 @@
 	import * as Card from '$lib/components/ui/card/';
 	import type { Snippet } from 'svelte';
 	import { Badge, GithubBadge } from '$lib/components/ui/badge';
+	import { X } from 'lucide-svelte';
+	import { cn } from '$lib/utils';
 
 	let props: {
 		title?: string;
@@ -12,26 +14,40 @@
 		children?: Snippet;
 		footer?: Snippet;
 		github?: string;
+		onremove?: () => void;
+		class?: string;
 	} = $props();
 </script>
 
 <a href={props.route}>
 	<Card.Root
-		class="
-		drop-shadow-xl
-		transition-transform hover:scale-105 
-		overflow-clip
-	"
+		class={cn(
+			'drop-shadow-xl transition-transform hover:scale-105 overflow-clip group',
+			props.class
+		)}
 	>
 		<AspectRatio ratio={1} class="flex flex-col">
 			<Card.Header>
 				{#if props.title}
-					<Card.Title>{props.title}</Card.Title>
+					<Card.Title class="mr-6">
+						{props.title}
+					</Card.Title>
 				{/if}
 				{#if props.description}
 					<Card.Description>{props.description}</Card.Description>
 				{/if}
 			</Card.Header>
+			{#if props.onremove}
+				<X
+					class="
+						absolute top-0 right-0 m-6 
+						text-muted-foreground hover:text-destructive 
+						opacity-0 transition-all duration-300
+						group-hover:opacity-100 group-focus-within:opacity-100 
+						"
+					onclick={props.onremove}
+				/>
+			{/if}
 			<Card.Content class="flex-grow min-h-0 min-w-0 p-0 m-6 overflow-clip rounded">
 				{@render props.children?.()}
 			</Card.Content>
