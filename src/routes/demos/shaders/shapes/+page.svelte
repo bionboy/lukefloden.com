9b2740e52ad10ad2f9b011029ba52e53895120cc
Luke Floden <luke.floden@gmail.com>
2025-03-25 16:17:46 -0400

    feat: get shapes frag working

---
+++ src/routes/demos/shaders/shapes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	// import type { PageData } from './$types';
+	import Shapes from './Shapes.svelte';
+
+	// let { data }: { data: PageData } = $props();
+</script>
+
+<!-- TODO: CLEAN UP -->
+<p>hi</p>
+<div class="debug w-screen h-screen m-4">
+	<Shapes />
+</div>
+<p>by</p>
---
+++ src/routes/demos/shaders/shapes/Shapes.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { WebGlShader } from 'svader';
+	import shaderShapes from '$lib/assets/shaders/shapes.frag?raw';
+
+	let mouseOverShapes: [number, number] = $state([-1, -1]);
+</script>
+
+<div class="size-full">
+	<WebGlShader
+		code={shaderShapes}
+		parameters={[
+			{ name: 'u_resolution', value: 'resolution' },
+			{ name: 'u_mouse', type: 'vec2', value: mouseOverShapes },
+			{ name: 'u_time', value: 'time' }
+		]}
+	>
+		<div class="fallback">WebGL not supported in this environment.</div>
+	</WebGlShader>
+</div>
