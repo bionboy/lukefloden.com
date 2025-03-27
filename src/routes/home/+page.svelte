9013bf57d02fcb2ca0af04d14a62a672c7509487
Luke Floden <luke.floden@gmail.com>
2025-03-27 17:31:52 -0400

    style (temp): user shader landing page for now

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -7,24 +7,46 @@
 	let { data }: { data: PageData } = $props();
 
 	import { setDocumentBodyTailwind } from '$lib/setBackground';
+	import MetaBalls from '../demos/shaders/meta-balls/MetaBalls.svelte';
+	import HoverGrid from '../demos/shaders/hover-grid/HoverGrid.svelte';
 	setDocumentBodyTailwind($mode !== ThemeMode.DARK ? 'bg-[hsl(180,100%,55%)]' : 'bg-accent');
 </script>
 
 {#snippet titleContent()}
-	{#if $mode === ThemeMode.LIGHT}
+	<!-- {#if $mode === ThemeMode.LIGHT}
 		<div class="absolute top-1/2 z-[1] scale-[1.8] sm:scale-[2.5] md:scale-[2.5] lg:scale-[3]">
 			<Cloud />
 		</div>
-	{/if}
-	<div class="title z-[2]">
+	{/if} -->
+
+	<div class="title z-[2] p-4 rounded-xl border-accent border-[1px] backdrop-blur-sm">
+		<!-- <div class="absolute inset-0 -z-[1] bg-transparent rounded-xl backdrop-blur-sm"> -->
+		<!-- -webkit-mask-image: radial-gradient(ellipse at center, black 0%, rgba(0, 0, 0, 0.8) 30%, transparent 60%); -->
+		<!-- mask-image: radial-gradient(ellipse at center, black 0%, rgba(0, 0, 0, 0.8) 30%, transparent 60%); -->
+		<!-- </div> -->
 		<h1>Welcome to my site...</h1>
-		<h1>things are in flux right now...</h1>
+		<h1>
+			things are in flux
+			<!-- right now -->
+			...
+		</h1>
 		<h1>but that's ok...</h1>
 	</div>
 {/snippet}
 
-<div class="hero bg-background">
+<!-- <div class="hero bg-background">
 	<Parallax {titleContent}></Parallax>
+</div> -->
+<div class="hero center-container overflow-clip">
+	<div class="aspect-square h-[max(110vh,110vw)]">
+		<!-- <Parallax {titleContent} /> -->
+		<MetaBalls />
+		<!-- <HoverGrid /> -->
+	</div>
+
+	<div class="fixed bottom-8 left-1/2 transform -translate-x-1/2 pointer-events-none">
+		{@render titleContent()}
+	</div>
 </div>
 <div class="after-hero mt-[--after-hero-offset]">
 	<!-- TODO(@bionboy, 2025-02-10): put some content here and add a gradient or something moving that shows the user they can scroll down -->
@@ -45,18 +67,22 @@
 		width: 100vw;
 	}
 
-	.title > h1 {
-		margin: 0px;
-		text-align: left;
-		/* TODO: pick a better fallback font to avoid layout shift */
-		font-family: 'Instrument Serif', cursive;
-		text-shadow: 0 0 30px;
+	.title {
+		background-color: oklch(from hsl(var(--accent)) calc(l * 0.8) c h / 0.2);
+		& > h1 {
+			@apply relative m-0 text-left;
 
-		@apply text-accent 
+			/* TODO: pick a better fallback font to avoid layout shift */
+			/* font-family: 'Instrument Serif', cursive; */
+			@apply font-jersey15;
+			/* text-shadow: 0 0 30px; */
+
+			@apply text-accent 
 			text-3xl sm:text-4xl  lg:text-5xl
 			/* leading-[4rem] */
-			font-normal
-			dark:text-4xl md:dark:text-5xl 
-			dark:leading-[4rem];
+			/* font-normal */
+			/* dark:text-4xl md:dark:text-5xl  */
+			/* dark:leading-[4rem] */;
+		}
 	}
 </style>
