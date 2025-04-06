2070564d343c4931bb87d2dd9b9e417260d6a115
Luke Floden <luke.floden@gmail.com>
2025-04-06 18:04:14 -0400

    chore (WIP): site header 2.5 wins, removing other versions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
 	import '../app.pcss';
 	import type { Snippet } from 'svelte';
-	import SiteHeader from './SiteHeader.svelte';
 	import { onNavigate } from '$app/navigation';
 	import { ModeWatcher } from '$lib/theme';
-	import SiteHeader2 from './SiteHeader2.svelte';
-	import SiteHeader3 from './SiteHeader3.svelte';
 	import SiteHeader25 from './SiteHeader25.svelte';
 
 	let { children }: { children: Snippet } = $props();
@@ -24,10 +21,7 @@
 
 <ModeWatcher />
 <div class="relative flex flex-col">
-	<!-- <SiteHeader /> -->
-	<!-- <SiteHeaderNew /> -->
 	<SiteHeader25 />
-	<!-- <SiteHeader headerStyle={Math.random() > 0.5 ? 'rounded' : 'default'} /> -->
 	{@render children()}
 </div>
 
--- src/routes/demos/+layout.svelte
+++ src/routes/demos/+layout.svelte
@@ -1,31 +1,12 @@
 <script lang="ts">
 	import bgImg from '$lib/assets/images/gradient-blur-texture.jpg?enhanced&blur=100';
 	import FullStaticBackground from '$lib/components/FullStaticBackground.svelte';
-	import Nav from '$lib/components/Nav.svelte';
-	import PageHeader from '$lib/components/PageHeader.svelte';
-	import { fade } from 'svelte/transition';
 
 	let { children } = $props();
-
-	const locations = [
-		{ route: '/demos/internal', name: 'Internal' },
-		{ route: '/demos/external', name: 'External' },
-		{ route: '/demos/shaders', name: 'Shaders' }
-	];
 </script>
 
 <FullStaticBackground imgSrc={bgImg} />
 
-<!-- TODO: fix the view transition so that the nav stays in place while the rest of the page slides -->
-<div class="asdf">
-	<PageHeader let:headerMinimized class="max-w-64 mx-auto" hideOnScroll={false}>
-		{#if !headerMinimized}
-			<div class="mx-auto" transition:fade={{ duration: 300 }}>
-				<Nav {locations} />
-			</div>
-		{/if}
-	</PageHeader>
-</div>
 <div class="page">
 	{@render children()}
 </div>
@@ -34,8 +15,5 @@
 	.page {
 		/* @apply relative flex flex-col; */
 		view-transition-name: page-content;
-	}
-	.asdf {
-		view-transition-name: header;
 	}
 </style>
