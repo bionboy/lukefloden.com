eb3493deb1b9be023ed7622c6b334f88c0e458d5
Luke Floden <luke.floden@gmail.com>
2025-04-07 12:51:39 -0400

    refactor: use better pattern for redirects of home and demo routes w/o hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,10 +2,8 @@ import { redirect, type Handle } from '@sveltejs/kit';
 // import { dev } from '$app/environment';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	const url = event.url;
-	middlewareRedirctFromBaseURL(url);
+	const url: URL = event.url;
 	middlewareUnderConstruction(url);
-	middlewareRedirectDemos(url);
 
 	return resolve(event);
 };
@@ -15,17 +13,5 @@ function middlewareUnderConstruction(url: URL) {
 	const isUnderConstruction = false;
 	if (isUnderConstruction && url.pathname !== '/under-construction') {
 		throw redirect(307, '/under-construction');
-	}
-}
-
-function middlewareRedirctFromBaseURL(url: URL) {
-	if (url.pathname === '/') {
-		throw redirect(302, '/home');
-	}
-}
-
-function middlewareRedirectDemos(url: URL) {
-	if (url.pathname === '/demos') {
-		throw redirect(302, '/demos/internal');
 	}
 }
