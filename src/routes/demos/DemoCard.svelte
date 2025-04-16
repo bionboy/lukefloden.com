4afab4e3f44855c44549d16010fbdbff2d46fa52
Luke Floden <luke.floden@gmail.com>
2025-04-16 17:02:52 -0400

    feat: external link icon styling for demo cards that link out of website

--- src/routes/demos/DemoCard.svelte
+++ src/routes/demos/DemoCard.svelte
@@ -3,7 +3,7 @@
 	import * as Card from '$lib/components/ui/card/';
 	import type { Snippet } from 'svelte';
 	import { Badge, GithubBadge } from '$lib/components/ui/badge';
-	import { X } from 'lucide-svelte';
+	import { ExternalLink, X } from 'lucide-svelte';
 	import { cn } from '$lib/utils';
 
 	let props: {
@@ -17,6 +17,9 @@
 		onremove?: () => void;
 		class?: string;
 	} = $props();
+
+	// if there is a remove action don't style for external
+	let external = $derived(props.onremove ? false : props.route?.startsWith('http'));
 </script>
 
 <a href={props.route}>
@@ -40,12 +43,24 @@
 			{#if props.onremove}
 				<X
 					class="
-						absolute top-0 right-0 m-6 
+						absolute top-4 right-4
 						text-muted-foreground hover:text-destructive 
 						opacity-0 transition-all duration-300
 						group-hover:opacity-100 group-focus-within:opacity-100 
 						"
+					size={24}
 					onclick={props.onremove}
+				/>
+			{/if}
+			{#if external}
+				<ExternalLink
+					class="
+				  absolute right-4 top-4 
+					text-muted-foreground 
+					transition-colors
+					group-hover:text-accent
+					"
+					size={24}
 				/>
 			{/if}
 			<Card.Content class="flex-grow min-h-0 min-w-0 p-0 m-6 overflow-clip rounded">
