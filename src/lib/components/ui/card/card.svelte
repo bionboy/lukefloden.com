5fda033226a918ecedd81ca9fcfe4c93a2bc0626
Luke Floden <luke.floden@gmail.com>
2025-08-28 20:23:55 -0400

    style: make cards slightly transparent by default

--- src/lib/components/ui/card/card.svelte
+++ src/lib/components/ui/card/card.svelte
@@ -9,7 +9,11 @@
 </script>
 
 <div
-	class={cn('bg-card text-card-foreground rounded-lg border shadow-sm', className)}
+	class={cn(
+		'bg-card text-card-foreground rounded-lg border shadow-sm',
+		'backdrop-blur-sm bg-opacity-50',
+		className
+	)}
 	{...$$restProps}
 >
 	<slot />
