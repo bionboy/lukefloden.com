0a2b01f1e2f7def18bdadeafb3564b4282f9a564
Luke Floden <luke.floden@gmail.com>
2025-08-28 20:28:55 -0400

    style: make section headers for portfolio muted

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -4,17 +4,17 @@
 	import DemosSection from './demos/DemosSection.svelte';
 </script>
 
-<div class="about max-w-7xl mx-auto my-4 p-4 flex flex-col gap-8 font-jersey15NOT">
-	<h1 class="gradient-text" id="projects">Projects</h1>
+<div class="max-w-7xl mx-auto">
+	<h1 id="projects">Projects</h1>
 	<ProjectsSection />
-	<h1 class="gradient-text" id="demos">Demos</h1>
+	<h1 id="demos">Demos</h1>
 	<DemosSection />
-	<h1 class="gradient-text" id="shaders">Shaders</h1>
+	<h1 id="shaders">Shaders</h1>
 	<ShadersSection />
 </div>
 
 <style lang="postcss">
 	h1 {
-		@apply leading-tight;
+		@apply leading-tight text-muted-foreground rounded-xl p-4;
 	}
 </style>
